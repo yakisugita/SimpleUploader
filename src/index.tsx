51b876fac8aa56567465cf9a4a325bd01b90a065
yakisugita <76557006+yakisugita@users.noreply.github.com>
2025-01-21 17:37:39 +0900

    R2のファイル数表示

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import { Hono } from "hono"
 import type { FC } from "hono/jsx"
 
+type Env =  {
+    R2_BUCKET: R2Bucket
+}
+
 const app = new Hono()
 
-app.get("/", (c) => {
-    return c.text("TOP PAGE")
+const Layout: FC = (props) => {
+    return (
+        <html>
+            <body>{props.children}</body>
+        </html>
+    )
+}
+
+
+app.get("/", async (c) => {
+    const list = await c.env.R2_BUCKET.list()
+    return c.text("TOP PAGE" + list.length)
 })
 
 export default app
