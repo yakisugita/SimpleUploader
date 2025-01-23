25e9b245f70b7055e83db67c56e013e5001c9b50
yakisugita <76557006+yakisugita@users.noreply.github.com>
2025-01-23 15:39:49 +0900

    ファイル名一覧表示テスト

--- src/index.tsx
+++ src/index.tsx
@@ -16,10 +16,26 @@ const Layout: FC = (props) => {
 }
 
 
+const Top: FC<{ files: any[] }> = (props: {
+    files: any[]
+  }) => {
+    return (
+      <Layout>
+        <h1>Hello Hono!</h1>
+        <ul>
+          {props.files.map((file) => {
+            return <li>{file.key}!!</li>
+          })}
+        </ul>
+      </Layout>
+    )
+  }
+
+
 app.get("/", async (c) => {
-    const list = await c.env.R2_BUCKET.list()
-    console.log(list)
-    return c.text("TOP PAGE" + list.length)
+    const r2List = await c.env.R2_BUCKET.list()
+    console.log(r2List.objects)
+    return c.html(<Top files={r2List.objects} />)
 })
 
 export default app
