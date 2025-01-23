99cb9673fba7ef59491fd97ec4950641050fc542
yakisugita <76557006+yakisugita@users.noreply.github.com>
2025-01-23 16:23:05 +0900

    R2への書き込み処理

--- src/index.tsx
+++ src/index.tsx
@@ -43,22 +43,25 @@ const Top: FC<{ files: any[] }> = (props: {
   }
 
 
-app.get("/", async (c) => {
+app.get("/", async (c:any) => {
     const r2List = await c.env.R2_BUCKET.list()
     console.log(r2List.objects)
     return c.html(<Top files={r2List.objects} />)
 })
 
-app.post("/", async (c) => {
+app.post("/", async (c:any) => {
     const body = await c.req.parseBody()
-    const file = body['file']
+    const file = body['file'] // File | string
     console.log(typeof(file))
-    // if (typeof(file) == "ob")
-    const blob = new Blob([body['file']], { type: body['file'].type })
-    console.log(file.type)
-    console.log(body)
-    console.log(body['file']) // File | string
-    return c.text("POST /")
+    if (typeof(file) == "object") {
+        const blob = new Blob([file], { type: file.type })
+        
+        await c.env.R2_HTML.put(file.name, blob)
+
+        return c.text("File Uploaded")
+    } else {
+        return c.text("Bad Request", 400)
+    }
 })
 
 export default app
